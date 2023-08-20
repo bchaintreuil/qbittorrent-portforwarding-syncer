ec9c59b4203a0b170f341bd3bf239c2a44b1ab3c
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-08-20 20:30:42 +0200

    final

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,20 @@
-FROM python:3
+FROM python:3-alpine
+
 WORKDIR /usr/src/app
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+RUN pip install --no-cache-dir -r requirements.txt
 
-ENTRYPOINT ["tail", "-f", "/dev/null"]
-#CMD [ "python", "main.py" ]
+COPY crontab /etc/crontabs/root
+RUN chown root:root /etc/crontabs/root && chmod 600 /etc/crontabs/root
+
+ENV GLUETUN_HOSTNAME=gluetun
+ENV GLUETUN_PORT=8000
+ENV QBITTORRENT_HOSTNAME=gluetun
+ENV QBITTORRENT_PORT=8080
+# qBittorrent default values
+ENV QBITTORRENT_USER=admin
+ENV QBITTORRENT_PWD=adminadmin
+
+ENTRYPOINT ["/usr/sbin/crond"]
+CMD ["-f", "-l", "6", "-L", "/dev/stdout"]
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
+#!/usr/bin/python3
 import qbittorrentapi
 import requests
+import os
 
 def main():
-    # First we retrieve the forwarded port from gluetun using the control server
-    r : requests.Response = requests.get(url="http://gluetun:8000/v1/openvpn/portforwarded")
+    # Initialization
+    try:
+        gluetun_hostname = os.environ['GLUETUN_HOSTNAME']
+        gluetun_port = os.environ['GLUETUN_PORT']
+        # Since qBittorrent is directly connected through the Gluetun container, we generally use the same hostname.
+        qbittorrent_hostname = os.getenv("QBITTORRENT_HOSTNAME", gluetun_hostname)
+        qbittorrent_port = os.environ['QBITTORRENT_PORT']
+        qbittorrent_user = os.getenv('QBITTORRENT_USER', "admin") # qBittorrent default values
+        qbittorrent_pwd = os.getenv('QBITTORRENT_PWD', "adminadmin") # qBittorrent default values
+    except KeyError:
+        print("One or more mandatory environment variables doesn't exist")
+    
+    # First we retrieve the assigned forwarding port from Gluetun using the control server
+    try:
+        gluetun_fwport = requests.get(url="http://{}:{}/v1/openvpn/portforwarded".format(gluetun_hostname, gluetun_port)).json()["port"]
+        print("Gluetun assigned forwarding port: " + gluetun_fwport)
+    except requests.exceptions.RequestException as e:
+        print("Gluetun connection error!")
+        exit(1)
 
-    print(r.json)
+    # We retrieve the qBittorrent set up one
+    with qbittorrentapi.Client(host=qbittorrent_hostname, port=qbittorrent_port, username=qbittorrent_user, password=qbittorrent_pwd) as qbt_client:
+        # display qBittorrent info
+        print(f"qBittorrent: {qbt_client.app.version}")
+        print(f"qBittorrent Web API: {qbt_client.app.web_api_version}")
+        for k, v in qbt_client.app.build_info.items():
+            print(f"{k}: {v}")
+
+        qbittorrent_fwport = qbt_client.app_preferences()["listen_port"]
+
+        if qbittorrent_fwport != gluetun_fwport:
+            print("Gluetun & qBittorrent ports are differents, setting up new port accordingly")
+            qbt_client.app_set_preferences(dict(listen_port=gluetun_fwport))
+        else:
+            print("Gluetun & qBittorrent ports are the same, nothing to do...")
 
 if __name__ == '__main__':
     main()
