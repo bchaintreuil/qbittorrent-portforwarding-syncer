ca841b71055708c6367f4ff41036bd2847f052aa
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-08-20 19:01:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 def main():
     # First we retrieve the forwarded port from gluetun using the control server
-    r : requests.Response = requests.get(url="http://localhost:9011/v1/openvpn/portforwarded")
+    r : requests.Response = requests.get(url="http://gluetun:8000/v1/openvpn/portforwarded")
 
     print(r.json)
 
